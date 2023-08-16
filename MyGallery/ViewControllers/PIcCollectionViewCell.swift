5f6b2490af452ef6be28e0e3fe7dd29fdeba0633
Никита Суровцев <nikitasurovcev@Nikitas-MacBook-Pro.local>
2023-08-16 18:25:44 +0300

    Unnecessary changes

---
+++ MyGallery/ViewControllers/PIcCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PIcCollectionViewCell.swift
+//  MyGallery
+//
+//  Created by Никита Суровцев on 16.08.23.
+//
+
+import UIKit
+
+class PIcCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
